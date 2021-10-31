0e3b41fd93b5c58bebc94d179b87fc070f01b5e9
taras.parfeniuk <taras.parfeniuk@gmail.com>
2021-10-31 18:40:46 +0200

    add result types

---
+++ src/PointOfSale.Results/ResultMessage.cs
@@ -0,0 +1,12 @@
+namespace PointOfSale.Results
+{
+    public class ResultMessage
+    {
+        public ResultMessage(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
